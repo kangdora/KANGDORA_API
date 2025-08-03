f10ffb7a785ea7383e3ffc9cc0893b2518127497
KangDora <ioprt123@gmail.com>
2025-08-03 16:08:25 +0900

    Init Spring Project

---
+++ src/main/java/kr/kangdora/portfolio_site/PortfolioSiteApplication.java
@@ -0,0 +1,13 @@
+package kr.kangdora.portfolio_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/kangdora/portfolio_site/PortfolioSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kangdora.portfolio_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
